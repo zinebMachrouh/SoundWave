98d58d77dab00a332c2bf2b84966208ce55b6b28
zinebMachrouh <zinebmac49@gmail.com>
2024-12-09 15:56:26 +0100

    initiate project

---
+++ src/main/java/org/example/soundwave/SoundWaveApplication.java
@@ -0,0 +1,13 @@
+package org.example.soundwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundWaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundWaveApplication.class, args);
+    }
+
+}
